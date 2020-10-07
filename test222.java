0f3b0556cea3ef7fe8383f5be9e34d2254a0cbc6
double x <52040234+mxxnbnb@users.noreply.github.com>
2020-10-07 10:22:00 +0800

    Update and rename test.java to test222.java

--- test222.java
+++ test222.java
@@ -3,5 +3,6 @@ package stu1;
 public class test {
 	public static void main(String[] args) {
 		System.out.print("efsfsdf");
-		System.out.printf("1")	}
+		System.out.printf("1")	
+	System.out.printf("Test")	}
 }
